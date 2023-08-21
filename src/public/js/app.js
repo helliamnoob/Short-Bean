307a5bafe0657cbf606b4b8c8dba664d699fe6e3
yerim <sola4718@gmail.com>
2023-08-21 13:01:18 +0900

    채팅 로직 구현하기 웹소켓 사용중

---
+++ src/public/js/app.js
@@ -0,0 +1,41 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+  }//닉네임을 Json 타입으로 변경하는 것
+
+function handleOpen() {
+  console.log("Connected to Server ✅");
+}
+socket.addEventListener("open", handleOpen);
+
+socket.addEventListener("message", (message) => {
+ const li = document.createElement("li");
+ li.innerText = message.data;
+ messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
