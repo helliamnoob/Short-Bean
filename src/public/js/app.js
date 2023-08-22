c2cf42cce84cca2bbb711efc6a606b29b9b1d0c7
yerim <sola4718@gmail.com>
2023-08-22 20:19:09 +0900

    소켓아이오

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,41 +1,15 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-function makeMessage(type, payload) {
-    const msg = { type, payload };
-    return JSON.stringify(msg);
-  }//닉네임을 Json 타입으로 변경하는 것
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-function handleOpen() {
-  console.log("Connected to Server ✅");
-}
-socket.addEventListener("open", handleOpen);
-
-socket.addEventListener("message", (message) => {
- const li = document.createElement("li");
- li.innerText = message.data;
- messageList.append(li);
-});
-
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server ❌");
-});
-
-function handleSubmit(event) {
+function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
   input.value = "";
 }
 
-function handleNickSubmit(event) {
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-    input.value = "";
-}
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -9,33 +9,46 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-function onSocketClose() {
-  console.log("Disconnected from the Browser ❌");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
-    socket.on("message", (msg) => {
-      const message = JSON.parse(msg);
-      switch (message.type) {
-        case "new_message":
-          sockets.forEach((aSocket) =>
-            aSocket.send(`${socket.nickname}: ${message.payload}`)
-          );
-        case "nickname":
-          socket["nickname"] = message.payload;
-      }
-    });
+wsServer.on("connection", (socket) => {
+socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
   });
+});
 
-  server.listen(3000, handleListen);
+
+
+//const wss = new WebSocket.Server({ server });
+
+// function onSocketClose() {
+//   console.log("Disconnected from the Browser ❌");
+// }
+
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser ✅");
+//     socket.on("close", onSocketClose);
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type) {
+//           case "new_message":
+//             sockets.forEach((aSocket) =>
+//               aSocket.send(`${socket.nickname}: ${message.payload}`)
+//             );break
+//           case "nickname":
+//             socket["nickname"] = message.payload;
+//         }
+//       });
+//     });
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
