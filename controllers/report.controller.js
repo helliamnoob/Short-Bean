b28c2458c2f5c043f1d7206755ce6c71ebe9a295
helloiamnoob <86592051+helliamnoob@users.noreply.github.com>
2023-08-22 10:01:56 +0900

    report 컨트롤러 완성
    report 컨트롤러 완성

--- controllers/report.controller.js
+++ controllers/report.controller.js
@@ -5,8 +5,10 @@ class ReportController {
 
     getReport = async (req, res, next) =>{
         try{
-  
-            res.status(200).json({data: })
+            const {userId} = res.locals.user;
+            const {reportId} = req.params;
+            const report = await this.reportService.getReport(reportId, userId);
+            res.status(200).json({data: report})
         }
         catch(error){
                 return res.status(500).json({message: error.message});
@@ -15,8 +17,11 @@ class ReportController {
 
     creatReport = async (req, res , next) => {
         try{
-   
-            res.satus(201).json({data:});
+            const {userId} = res.locals.user;
+            const {adminId} = req.params;
+            const {reportContent, reportUserId, reportstatus} = req.body;
+            const report = await this.reportService.creatReport(adminId, userId, reportContent, reportUserId, reportstatus)
+            res.satus(201).json({data:report});
         }
         catch(error){
             return res.status(500).json({message: error.message});
@@ -24,9 +29,12 @@ class ReportController {
     }
     updateReport = async(req, res, next) => {
         try{
-    
-            
-            res.status(200).json({data:});
+            const {userId} = res.locals.user;
+            const {adminId} = req.params;
+            const {reportContent} = req.body;
+            const report = await this.reportService.getReport(adminId, userId, reportContent);
+
+            res.status(200).json({data:report});
         }
         catch(error){
             return res.status(500).json({message: error.message})
@@ -34,9 +42,10 @@ class ReportController {
     }
     deleteReport = async(req, res,next) => {
         try{
-            
-            
-            res.status(200).json({data: });
+            const {userId} = res.locals.user;
+            const {reportId} = req.params;
+            const report = await this.reportService.deleteReport(reportId, userId, reportContent);
+            res.status(200).json({data: report});
         }
         catch(error){
             return res.status(500).json({message:error.message});
