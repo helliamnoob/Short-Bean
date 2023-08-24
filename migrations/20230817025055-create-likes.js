296ae88083c4bdbc73053e08bdb8d9d54dbc76d5
yerim <sola4718@gmail.com>
2023-08-24 19:14:17 +0900

    풀 용 커밋

--- migrations/20230817025055-create-likes.js
+++ migrations/20230817025055-create-likes.js
@@ -7,37 +7,37 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.BIGINT
+        type: Sequelize.BIGINT,
       },
       post_id: {
         allowNull: false,
         type: Sequelize.BIGINT,
         references: {
-          model: "Posts",
-          key: "post_id",
+          model: 'Posts',
+          key: 'post_id',
         },
       },
       user_id: {
         allowNull: false,
         type: Sequelize.BIGINT,
         references: {
-          model: "Users",
-          key: "user_id",
+          model: 'Users',
+          key: 'user_id',
         },
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.fn("now"),
+        defaultValue: Sequelize.fn('now'),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.fn("now"),
-      }
+        defaultValue: Sequelize.fn('now'),
+      },
     });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Likes');
-  }
+  },
 };
