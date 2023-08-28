43a219b96b768f68161db1fb4df54951ab1fa0d7
k0723 <kimbum7824@gmail.com>
2023-08-28 12:17:32 +0900

    feat

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,6 +5,9 @@ const middleware = require('../middlewares/auth_middleware');
 const router = express.Router();
 const { Op } = require('sequelize');
 const cache = require('node-cache');
+const my_cache = new node_cache({stdTTL: 200, checkperiod:600});
+const axios = require('axios');
+const crypto= require('crypto-js')
 const cache_middleware = require("../middlewares/cache_middleware");
 require('dotenv').config();
 
