a18f530f704c50165c16297ae65e07e9adf938e9
yerim <sola4718@gmail.com>
2023-08-30 00:36:58 +0900

    소켓 아이디로 맵핑, 로그인시 바로 소켓 연결, 초대장이 성공과 실패를 반복함

--- face.socket.js
+++ face.socket.js
@@ -20,16 +20,16 @@ const faceSocketController = (io) => {
             }
         });
 
-      io.on('connection', (socket) => {
-    socket.on('accept_face_chat', (inviterSocketId) => {
+       // 초대를 수락했을 때 처리하는 로직
+       // 초대를 수락한 사용자와 초대를 발송한 사용자에게 이벤트 전송
+       // 기존의 'accept_face_chat' 이벤트 핸들러 삭제 후 아래의 로직으로 대체합니다.
+       socket.on('accept_face_chat', (inviterSocketId) => {
         // 초대를 수락한 사용자에게 이벤트 전송
-        socket.emit('start_face_chat');
+        io.to(socket.id).emit('start_face_chat');
         
-        // 초대를 발송한 사용자에게 이벤트 전송
+        // 초대를 발송한 사용자에게도 같은 메세지 전송
         io.to(inviterSocketId).emit('start_face_chat');
     });
-});
-
         socket.on("join_room", (roomName) => {
             socket.join(roomName);
             socket.to(roomName).emit("welcome");
@@ -50,7 +50,19 @@ const faceSocketController = (io) => {
         socket.on("ice", (ice, roomName) => {
             socket.to(roomName).emit("ice", ice);
         });
+            // 클라이언트 연결이 끊어졌을 때 처리하는 로직
+        socket.on('disconnect', () => {
+        console.log('user disconnected:', socket.id);
+        for(let userId in userSockets){
+            if(userSockets[userId] === socket.id){
+                delete userSockets[userId];
+            }
+        }
+        console.log(userSockets);  // 콘솔 출력으로 현재 남아있는 유저들 확인 
+     });
     });
+
+
 };
 
 module.exports = faceSocketController;
--- public/js/invite.js
+++ public/js/invite.js
@@ -1,20 +1,48 @@
-const socket = io();
+document.addEventListener('DOMContentLoaded', () => {
+    const jwtToken = getCookieValue('authorization');
+    if (!jwtToken) {
+        alert('로그인 후 이용가능한 서비스입니다.');
+        window.location.href = `/public/views/login.html`;
+    } else {
+        const socket = io({
+            auth: {
+                token: jwtToken,
+            },
+        });
 
-const currentUserId = 1; 
+        // JWT 토큰에서 사용자 ID 추출
+        const base64Url = jwtToken.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        
+         console.log("base64:", base64);  // 콘솔
 
-socket.emit('register', currentUserId);
+         const payloadObj = JSON.parse(window.atob(base64));
+         
+         console.log("payloadObj:", payloadObj);  // 콘솔
 
+         const currentUserId = payloadObj.user_id;  // 현재 로그인한 사용자의 ID
+         
+         console.log("userId:", currentUserId);  // 콘솔
 
-// 화상 채팅 초대 버튼 이벤트 리스너
-document.body.addEventListener('click', (event) => {
-    if (event.target.classList.contains('facechatBtn')) {
-        const userId = event.target.getAttribute('data-user-id');
-        socket.emit('invite_face_chat', userId);
-        console.log(`Invitation sent to user with ID: ${userId}`);
-    }
-});
+          socket.emit('register', currentUserId);
 
-// 초대 수신 시의 이벤트 처리
+          // 화상 채팅 초대 버튼 이벤트 리스너
+          document.body.addEventListener('click', (event) => {
+              if (event.target.classList.contains('facechatBtn')) {
+                  const inviteeId = event.target.getAttribute('data-user-id');
+
+                  // 로그인한 사용자가 자기 자신에게 화상채팅을 걸 수 없도록 체크
+                  if (inviteeId === String(currentUserId)) { 
+                      alert("자기 자신에게는 화상채팅을 할 수 없습니다.");
+                      return;
+                  }
+
+                  socket.emit('invite_face_chat', inviteeId);
+                  console.log(`Invitation sent to user with ID: ${inviteeId}`);
+              }
+          });
+
+        // 초대 수신 시의 이벤트 처리
 socket.on("receive_invite", (inviterSocketId) => {
     const accept = confirm("화상 채팅 초대가 도착했습니다! 수락하시겠습니까?"); 
     if (accept) {
@@ -27,6 +55,14 @@ socket.on("start_face_chat", () => {
     console.log("Invitation accepted! Start the face chat.");
     window.location.href = "/facechat";  // 화상 채팅 페이지 URL을 가정한 것입니다.
 });
+     }
+});
 
+function getCookieValue(cookieName) {
+     const cookieParts = document.cookie.split('; ');
 
-
+     for (const part of cookieParts) {
+       const [name, value] = part.split('=');
+       if (name === cookieName) { return value; }
+    }
+}
