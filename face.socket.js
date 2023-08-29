a9e1920acef544399f6cf71e5408267d703eb32a
yerim <sola4718@gmail.com>
2023-08-29 17:37:49 +0900

    화상채팅 초대장 발송까지는 확인

--- face.socket.js
+++ face.socket.js
@@ -1,28 +1,29 @@
-const tutorSockets = {};
-
 const faceSocketController = (io) => {
+    let userSockets = {}; // { userId: socketId }
+
     io.on("connection", (socket) => {
         console.log('a user connected:', socket.id);
+
+        // 사용자가 연결될 때, userSockets 에 사용자 ID와 소켓 ID 저장
+        socket.on("register", (userId) => {
+            userSockets[userId] = socket.id;
+        });
 
         socket.on("join_room", (roomName) => {
             socket.join(roomName);
             socket.to(roomName).emit("welcome");
         });
 
-        // 튜터가 접속했을 때 해당 튜터의 ID와 소켓을 저장
-        socket.on('register_tutor', (tutor_id) => {
-            tutorSockets[tutor_id] = socket.id;
-        });
-
-        socket.on("invite_face_chat", (tutor_id) => {
-            const tutorSocketId = tutorSockets[tutor_id];
+        // 튜터에게 초대장 보내기
+        socket.on('invite_face_chat', (user_id) => {
+            const tutorSocketId = userSockets[user_id];
             if (tutorSocketId) {
-                io.to(tutorSocketId).emit("receive_invite", socket.id);
-            }
+                io.to(tutorSocketId).emit('receive_invite', socket.id);  // 초대한 사람의 소켓 ID 전송
+            }console.log(user_id);
         });
 
-        socket.on("accept_face_chat", (user_id) => {
-            io.to(user_id).emit("start_face_chat");
+        socket.on("accept_face_chat", (inviterSocketId) => {
+            io.to(inviterSocketId).emit("start_face_chat");
         });
 
         socket.on("offer", (offer, roomName) => {
@@ -40,3 +41,34 @@ const faceSocketController = (io) => {
 };
 
 module.exports = faceSocketController;
+
+
+// //원래로직
+// const faceSocketController = (io) => {
+//     io.on("connection", (socket) => {
+//         console.log('a user connected:', socket.id);
+
+//         socket.on("join_room", (roomName) => {
+//             socket.join(roomName);
+//             socket.to(roomName).emit("welcome");
+//         });
+
+//         socket.on("accept_face_chat", (user_id) => {
+//             io.to(user_id).emit("start_face_chat");
+//         });
+
+//         socket.on("offer", (offer, roomName) => {
+//             socket.to(roomName).emit("offer", offer);
+//         });
+
+//         socket.on("answer", (answer, roomName) => {
+//             socket.to(roomName).emit("answer", answer);
+//         });
+
+//         socket.on("ice", (ice, roomName) => {
+//             socket.to(roomName).emit("ice", ice);
+//         });
+//     });
+// };
+
+// module.exports = faceSocketController;
--- public/js/invite.js
+++ public/js/invite.js
@@ -2,20 +2,29 @@ const socket = io();
 
 // 화상 채팅 초대 버튼 이벤트 리스너
 document.body.addEventListener('click', (event) => {
-    // 이벤트 위임을 사용하여 facechatBtn이 클릭되었는지 확인
     if (event.target.classList.contains('facechatBtn')) {
-        const userId = event.target.getAttribute('data-user-id'); // 'value' 대신 'data-user-id' 속성을 사용합니다.
+        const userId = event.target.getAttribute('data-user-id');
         socket.emit('invite_face_chat', userId);
-        console.log(userId);
+        console.log(`Invitation sent to user with ID: ${userId}`);
     }
 });
 
 // 초대 수신 시의 이벤트 처리
-socket.on("receive_invite", (userSocketId) => {
+socket.on("receive_invite", (inviterSocketId) => {
+    console.log("Received an invite!");
     document.getElementById('inviteAlert').style.display = 'block';
+    
     document.getElementById('acceptInvite').addEventListener('click', () => {
-        socket.emit('accept_face_chat', userSocketId);
+        socket.emit('accept_face_chat', inviterSocketId);
         document.getElementById('inviteAlert').style.display = 'none';
     });
 });
+
+// 초대가 수락되었을 때 처리하는 이벤트 핸들러
+socket.on("accept_response", () => {
+    console.log("Invitation accepted! Start the face chat.");
+    // 화상 채팅 시작 로직 구현
+});
+
+
 
